b4a67d0d7e101c2088a2fe03aac3f2a93cddf359
MeenaSivakumar <meenasad09@gmail.com>
2024-12-12 21:34:03 +0530

    Sign Up Ui completed

--- pages/signup_page.tsx
+++ pages/signup_page.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import type {} from 'react';
-import {StyleSheet, Image} from 'react-native';
+import {StyleSheet, Image, Text} from 'react-native';
 import {
   Center,
   Box,
@@ -9,8 +10,10 @@ import {
   FormControl,
   Input,
   Button,
+  HStack,
 } from 'native-base';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const SignUpPage = () => {
   return (
@@ -24,21 +27,54 @@ const SignUpPage = () => {
           <Heading style={styles.heading}>Create Account</Heading>
           <VStack space={2} mt="2">
             <FormControl>
-              <Input style={styles.input} variant="underlined" placeholder="Full Name" inlineImageLeft='' />
+              <HStack style={styles.inputContainer}>
+                <Icon name="user" size={25} style={styles.icon} />
+                <Input
+                  style={styles.input}
+                  variant="underlined"
+                  placeholder="Full Name"
+                />
+              </HStack>
             </FormControl>
             <FormControl>
-              <Input style={styles.input}  variant="underlined" placeholder="Email" />
+              <HStack style={styles.inputContainer}>
+                <Icon name="envelope" size={22} style={styles.icon} />
+                <Input
+                  style={styles.input}
+                  variant="underlined"
+                  placeholder="Email"
+                />
+              </HStack>
             </FormControl>
             <FormControl>
-              <Input
-              style={styles.input}
-                variant="underlined"
-                type="password"
-                placeholder="password"
-              />
+              <HStack style={styles.inputContainer}>
+                <Icon name="lock" size={30} style={styles.icon} />
+                <Input
+                  style={styles.input}
+                  variant="underlined"
+                  type="password"
+                  placeholder="password"
+                />
+              </HStack>
             </FormControl>
             <Button style={styles.button}>Sign up</Button>
           </VStack>
+          <HStack style={styles.logoContainer}>
+            <Image
+              style={styles.logo}
+              source={require('../assets/google.png')}
+            />
+            <Image
+              style={styles.logo}
+              source={require('../assets/linkdin.png')}
+            />
+          </HStack>
+          <HStack style={styles.loginText}>
+            <Text style={styles.text}>
+              Already User?
+              <Text style={{color: '#0A6F8E'}}>Log In</Text>
+            </Text>
+          </HStack>
         </Box>
       </Center>
     </SafeAreaView>
@@ -50,25 +86,58 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
     marginLeft: 50,
-    
   },
   heading: {
-    marginBottom: 10,
     color: '#0089B3',
     fontSize: 30,
-    fontWeight:900,
-    fontFamily:'inter',
+    fontWeight: 900,
+    fontFamily: 'inter',
+    textAlign: 'center',
+    marginBottom: 15,
   },
-  input:{
-    fontSize:18,
-    color:'#999999',
-    fontFamily:'inter',
-    fontWeight:'bold',
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 8,
+    borderColor: '#E0E0E0',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    height: 60,
+    width: 300,
   },
-  button:{
-    marginTop:25,
-    color:'#04A8DB',
-  }
+  input: {
+    fontSize: 15,
+    fontFamily: 'inter',
+    fontWeight: 'bold',
+    marginRight: 50,
+  },
+  icon: {
+    marginRight: 15,
+    color: '#999999',
+  },
+  button: {
+    marginTop: 10,
+    color: '#0A6F8E',
+    width: 100,
+    marginLeft: 100,
+  },
+  logo: {
+    width: 30,
+    height: 30,
+    margin: 30,
+  },
+  logoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  loginText: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 15,
+    fontFamily: 'inter',
+  },
 });
 
 export default SignUpPage;
