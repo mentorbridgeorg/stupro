726407deaa88f730441e1a578fe2d93f0c859f0e
MeenaSivakumar <meenasad09@gmail.com>
2024-12-14 21:57:59 +0530

    SignUp Completed

--- pages/signup_page.tsx
+++ pages/signup_page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
 import type {} from 'react';
-import {StyleSheet, Image, Text, Pressable} from 'react-native';
+import {StyleSheet, Image, Text, Pressable, ScrollView} from 'react-native';
 import {
   Center,
   Box,
@@ -13,127 +13,280 @@ import {
   Stack,
   Checkbox,
   Divider,
+  WarningOutlineIcon,
 } from 'native-base';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const SignUpPage = () => {
+  //setstate for userInfo
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+
+  //setState for handling user infoErrors
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [checkError, setCheckError] = useState('');
+
+  //Form validation
+  const validateForm = () => {
+    let isValid = true;
+
+    //firstNmae validation
+    if (!firstName.trim()) {
+      setFirstNameError('First Name is Required');
+      isValid = false;
+    } else {
+      setFirstNameError('');
+    }
+
+    //LastName validation
+    if (!lastName.trim()) {
+      setLastNameError('last Name is Required');
+      isValid = false;
+    } else {
+      setLastNameError('');
+    }
+
+    //email validation
+    if (!email.trim()) {
+      setEmailError('Email Address is Required');
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError('Enter a Valid Email Address');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+
+    //password validation
+    if (!password.trim()) {
+      setPasswordError('Password is Required');
+      isValid = false;
+    } else if (password.length < 8) {
+      //length of pasword
+      setPasswordError('Password must be at least 8 characters');
+      isValid = false;
+    } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      // checking special characters
+      setPasswordError('Password must include at least one special character');
+      isValid = false;
+    } else {
+      setPasswordError('');
+    }
+
+    //validation for accepting terms & conditon
+    if (!isChecked) {
+      setCheckError('You Must Agree to the terms and conditions.');
+      isValid = false;
+    } else {
+      setCheckError('');
+    }
+    return isValid;
+  };
+
+  const handleSingnUp = async () => {
+    if (validateForm()) {
+      setLoading(true);
+      try {
+        console.log('user registered');
+      } catch (error) {
+        console.error('error registering user');
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
   return (
-    <SafeAreaView>
-      <Image
-        source={require('../assets/signup_logo.png')}
-        style={styles.image}
-      />
-      <Center>
-        <Box w="90%">
-          <Heading style={styles.heading}>Welcome To StuPro</Heading>
-          <Text
-            style={styles.subHead}>
-            Create Account
-          </Text>
-          <Stack space={6} mt="2">
-            <FormControl mt="10">
-              <HStack justifyContent="space-between">
-                <Text style={[styles.label, {flex: 1, textAlign: 'left'}]}>
-                  First Name
-                </Text>
-                <Text style={[styles.label, {flex: 1, textAlign: 'left'}]}>
-                  Last Name
-                </Text>
-              </HStack>
-              <HStack space={2} mt="1">
+    <ScrollView
+      contentContainerStyle={{flexGrow: 1}}
+      keyboardShouldPersistTaps="handled">
+      <SafeAreaView>
+        {/* {signup Image} */}
+        <Image
+          source={require('../assets/signup_logo.png')}
+          style={styles.image}
+        />
+        <Center>
+          <Box w="90%">
+            {/* heading */}
+            <Heading style={styles.heading}>Welcome To StuPro</Heading>
+            <Text style={styles.subHead}>Create Account</Text>
+            {/* Signup Form */}
+            <Stack space={6} mt="2">
+              <FormControl
+                mt="10"
+                isInvalid={!!firstNameError || !!lastNameError}>
+                <HStack justifyContent="space-between">
+                  {/* First and Last Name Label */}
+                  <Text style={[styles.label, {flex: 1, textAlign: 'left'}]}>
+                    First Name
+                  </Text>
+                  <Text style={[styles.label, {flex: 1, textAlign: 'left'}]}>
+                    Last Name
+                  </Text>
+                </HStack>
+                <HStack space={2} mt="1">
+                  {/* First and Last Name TextInput */}
+                  <Input
+                    borderColor={'#0089B3'}
+                    flex={1}
+                    style={styles.input}
+                    InputLeftElement={
+                      <Icon name="user" size={22} style={styles.icon} />
+                    }
+                    value={firstName}
+                    onChangeText={setFirstName}
+                    placeholder="First Name"
+                  />
+
+                  <Input
+                    borderColor={'#0089B3'}
+                    flex={1}
+                    style={styles.input}
+                    InputLeftElement={
+                      <Icon name="user" size={22} style={styles.icon} />
+                    }
+                    value={lastName}
+                    onChangeText={setLastName}
+                    placeholder="Last Name"
+                  />
+                </HStack>
+                <HStack justifyContent="space-between">
+                  {/* First and Last Name Error Message*/}
+                  <FormControl.ErrorMessage
+                    leftIcon={<WarningOutlineIcon size="xs" />}>
+                    {firstNameError ? <Text>{firstNameError}</Text> : null}
+                  </FormControl.ErrorMessage>
+                  <FormControl.ErrorMessage
+                    leftIcon={<WarningOutlineIcon size="xs" />}>
+                    {lastNameError ? (
+                      <Text style={{flex: 1, textAlign: 'left'}}>
+                        {lastNameError}
+                      </Text>
+                    ) : null}
+                  </FormControl.ErrorMessage>
+                </HStack>
+              </FormControl>
+              <FormControl isInvalid={!!emailError}>
+                {/* Email Address Label */}
+                <Text style={styles.label}>Email Address</Text>
+                {/* Email Address TextInput */}
                 <Input
                   borderColor={'#0089B3'}
-                  flex={1}
-                  style={styles.input}
+                  style={styles.inputtext}
                   InputLeftElement={
-                    <Icon name="user" size={22} style={styles.icon} />
+                    <Icon name="envelope" size={22} style={styles.icon} />
                   }
-                  placeholder="First Name"
+                  value={email}
+                  onChangeText={setEmail}
+                  placeholder="Email"
                 />
+                <FormControl.ErrorMessage
+                  leftIcon={<WarningOutlineIcon size="xs" />}>
+                  {/* Email Address Error*/}
+                  {emailError ? <Text>{emailError}</Text> : null}
+                </FormControl.ErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!!passwordError}>
+                {/* Password Label*/}
+                <Text style={styles.label}>Password</Text>
+                {/* Password TextInput*/}
                 <Input
                   borderColor={'#0089B3'}
-                  flex={1}
-                  style={styles.input}
+                  style={styles.inputtext}
                   InputLeftElement={
-                    <Icon name="user" size={22} style={styles.icon} />
+                    <Icon name="lock" size={30} style={styles.icon} />
                   }
-                  placeholder="Last Name"
+                  type={show ? 'text' : 'password'}
+                  InputRightElement={
+                    <Pressable onPress={() => setShow(!show)}>
+                      <Icon
+                        name={show ? 'eye' : 'eye-slash'}
+                        size={22}
+                        style={styles.icon}
+                      />
+                    </Pressable>
+                  }
+                  value={password}
+                  onChangeText={setPassword}
+                  placeholder="Password"
                 />
-              </HStack>
-            </FormControl>
-            <FormControl>
-              <Text style={styles.label}>Email Address</Text>
-              <Input
-                borderColor={'#0089B3'}
-                style={styles.inputtext}
-                InputLeftElement={
-                  <Icon name="envelope" size={22} style={styles.icon} />
-                }
-                placeholder="Email"
-              />
-            </FormControl>
-            <FormControl>
-              <Text style={styles.label}>Password</Text>
-              <Input
-                borderColor={'#0089B3'}
-                style={styles.inputtext}
-                InputLeftElement={
-                  <Icon name="lock" size={30} style={styles.icon} />
-                }
-                type={show ? 'text' : 'password'}
-                InputRightElement={
-                  <Pressable onPress={() => setShow(!show)}>
-                    <Icon
-                      name={show ? 'eye-slash' : 'eye-slash'}
-                      size={22}
-                      style={styles.icon}
-                    />
-                  </Pressable>
-                }
-                placeholder="Password"
-              />
-            </FormControl>
+                <FormControl.ErrorMessage
+                  leftIcon={<WarningOutlineIcon size="xs" />}>
+                  {/* Password Error Message*/}
+                  {passwordError ? <Text>{passwordError}</Text> : null}
+                </FormControl.ErrorMessage>
+              </FormControl>
+            </Stack>
+            {/* Terms & conditions checkbox*/}
+            <Checkbox
+              isChecked={isChecked}
+              onChange={setIsChecked}
+              color="green"
+              value={''}
+              mt="6">
+              By checking, you agree to the terms and conditions & privacy
+              policy.
+            </Checkbox>
+            {/* checkBox Error*/}
+            {checkError ? (
+              <>
+                <Text style={{color: 'red', marginTop: 5}}>
+                  <Icon name="warning" color="red" /> {checkError}
+                </Text>
+              </>
+            ) : null}
+
+            {/* Signup Button*/}
             <Button
+              isLoading={loading}
               style={styles.button}
-              _text={{fontWeight: 'bold', fontSize: 18}}>
+              _text={{fontWeight: 'bold', fontSize: 18}}
+              onPress={handleSingnUp}>
               Sign up
             </Button>
-          </Stack>
-          <Checkbox color="green" value={''}>
-            By Signingup,you are agree to the terms and conditions & privacy
-            policy
-          </Checkbox>
-          <HStack space={5} mt="6" justifyContent="space-between">
-            <Divider
-              width="150"
-              _light={{
-                bg: 'muted.800',
-              }}
-            />
-            <Text>Or</Text>
-            <Divider
-              width="150"
-              _light={{
-                bg: 'muted.800',
-              }}
-            />
-          </HStack>
-          <Text style={styles.text}>Sign in With</Text>
-          <HStack style={styles.logoContainer}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/google.png')}
-            />
-            <Image
-              style={styles.logo}
-              source={require('../assets/linkdin.png')}
-            />
-          </HStack>
-        </Box>
-      </Center>
-    </SafeAreaView>
+
+            <HStack space={5} mt="6" justifyContent="space-between">
+              <Divider
+                width="150"
+                _light={{
+                  bg: 'muted.800',
+                }}
+              />
+              <Text>Or</Text>
+              <Divider
+                width="150"
+                _light={{
+                  bg: 'muted.800',
+                }}
+              />
+            </HStack>
+
+            <Text style={styles.text}>Sign in With</Text>
+            {/* Sign in Logo*/}
+            <HStack style={styles.logoContainer}>
+              <Image
+                style={styles.logo}
+                source={require('../assets/google.png')}
+              />
+              <Image
+                style={styles.logo}
+                source={require('../assets/linkdin.png')}
+              />
+            </HStack>
+          </Box>
+        </Center>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
@@ -150,14 +303,14 @@ const styles = StyleSheet.create({
   //   borderRadius: 5
   // },
   heading: {
-    margin:5,
+    margin: 5,
     color: '#0089B3',
     fontSize: 30,
     fontFamily: 'Lusitana-Bold',
     textAlign: 'center',
     marginBottom: 15,
   },
-  subHead:{
+  subHead: {
     fontSize: 20,
     fontWeight: '700',
     fontFamily: 'Lusitana-Regular',
@@ -172,26 +325,26 @@ const styles = StyleSheet.create({
     fontSize: 13,
     fontFamily: 'inter',
     fontWeight: 'bold',
-    backgroundColor:'#FFFFFF',
+    backgroundColor: '#FFFFFF',
   },
   inputtext: {
     fontSize: 15,
     fontFamily: 'inter',
     fontWeight: 'bold',
-    backgroundColor:'#FFFFFF',
+    backgroundColor: '#FFFFFF',
   },
   icon: {
     padding: 8,
     color: '#999999',
-    backgroundColor:'#FFFFFF',
+    backgroundColor: '#FFFFFF',
   },
   button: {
     color: '#0A6F8E',
-    width: 100,
+    width: 130,
     alignSelf: 'center',
-    borderRadius: 10,
-    borderWidth: 1,
-    marginBottom: 10,
+    borderRadius: 30,
+    marginTop: 10,
+    marginBottom: 5,
   },
   logo: {
     width: 30,
