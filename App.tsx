27d64f57ff93d65bf17242a1c2926834695d0795
kiruthii <kiruthikaganesan757@gmail.com>
2024-12-24 22:15:00 +0530

    Loader Provider added in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import { LoadingProvider } from './src/context/LoaderStateManagement/LoadingContext';
-import LoaderComponent from './src/components/Loader/LoaderComponent';
-
-const App = () => {
-  return (
-    <LoadingProvider>
-      //Navigation of all pages
-      
-      <LoaderComponent />
-    </LoadingProvider>
-  );
-};
-
-export default App;
-
 
