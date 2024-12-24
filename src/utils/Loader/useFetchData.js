aa564c36004633da24d42cd92b303c858c2c8536
kiruthii <kiruthikaganesan757@gmail.com>
2024-12-24 22:18:44 +0530

    fetch data while loading

--- src/utils/Loader/useFetchData.js
+++ src/utils/Loader/useFetchData.js
@@ -1,1 +1,16 @@
+const fetchData = async (showLoading, hideLoading) => {
+  console.log("Loading started");
+  showLoading(); // Show loader
 
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); //  2-second delay
+    console.log('Data loaded successfully!');
+  } catch (error) {
+    console.log('Error', 'Failed to load data');
+  } finally {
+    console.log("Loading finished");
+    hideLoading(); // Hide loader
+  }
+};
+
+export default fetchData;
