548c636b3356f8bedf8cdfa412206d126fe41ed7
Vishwadharshinivinod <vishwadharshini1793@gmail.com>
2025-01-05 15:15:32 -0500

    added preference page in app.tsx

--- App.tsx
+++ App.tsx
@@ -5,19 +5,19 @@
  * @format
  */
 
-import {NativeBaseProvider} from 'native-base';
+//import {NativeBaseProvider} from 'native-base';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import {SignIn} from './src/pages/SignIn';
+import {Preferences} from './src/pages/Preferences';
 import {theme} from './src/theme';
 
 function App(): React.JSX.Element {
   return (
-    <NativeBaseProvider theme={theme}>
-      <SafeAreaView>
-        <SignIn />
+    //<NativeBaseProvider theme={theme}>
+      <SafeAreaView style ={{flex : 1}}>
+        <Preferences />
       </SafeAreaView>
-    </NativeBaseProvider>
+    //</NativeBaseProvider>
   );
 }
 
