7b2047ebfca2eac977cb60eed07340dd6ac9cfb3
SBKrishnan <balakrishnan19211@gmail.com>
2025-01-21 21:02:20 +0530

    In Option reuseable component , adding onPress props

--- src/pages/Profile/components/Options.tsx
+++ src/pages/Profile/components/Options.tsx
@@ -2,17 +2,16 @@ import React, {Component} from 'react';
 import {Pressable, Box} from 'native-base';
 import {ArrowRight} from '../../../assets/icons/ArrowRight';
 
-const Options = ({Component}: any) => {
+const Options = ({Component, onPress}: any) => {
   return (
     <Pressable
-      mt={4}
       marginX={4}
       backgroundColor={'yellow.200'}
       p={3}
       borderRadius={15}
       flexDirection="row"
       justifyContent="space-between"
-      onPress={() => {}}>
+      onPress={onPress}>
       <Box
         marginX={5}
         _text={{fontSize: 'xl', fontWeight: 'bold', color: 'black'}}>
