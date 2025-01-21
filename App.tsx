1d7a8a772be78c1dd52e0cecde657a24e5f8897c
SBKrishnan <balakrishnan19211@gmail.com>
2025-01-21 15:58:01 +0530

    create Navigation

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import {NativeBaseProvider} from 'native-base';
+import {NativeBaseProvider, Stack} from 'native-base';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {theme} from './src/theme';
@@ -13,14 +13,20 @@ import {SignUp} from './src/pages/SignUp/SignUp';
 import {SignIn} from './src/pages/SignIn';
 import {Preferences} from './src/pages/Preferences';
 import {Home} from './src/pages/Home';
+import {Profile} from './src/pages/Profile';
+import EditProfile from './src/pages/Profile/EditProfile';
+import {NavigationContainer} from '@react-navigation/native';
+import {RootStack} from './src/navigation/RootStack';
 
 function App(): React.JSX.Element {
   return (
-    <NativeBaseProvider theme={theme}>
-      <SafeAreaView>
-        <Home />
-      </SafeAreaView>
-    </NativeBaseProvider>
+    <>
+      <NativeBaseProvider theme={theme}>
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </>
   );
 }
 
