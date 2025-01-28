f6ad9a76062c9cbe92df05d27920c39f357cb47a
Nithyabala2004 <144366758+Nithyabala2004@users.noreply.github.com>
2025-01-28 16:06:07 +0530

    Categoriespage  added (#24)
    Co-authored-by: Mentor Bridge <mentorbridgeindia@gmail.com>

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import Options from './components/Options';
 
 export const Profile = () => {
+   
   const navigation = useNavigation();
   const Position = [
     {value: 'Student', background: 'lightblue'},
@@ -47,8 +48,11 @@ export const Profile = () => {
         Settings
       </Box>
       <Box marginY={5}>
-        <Options Component="Change password" onPress={() => {}} />
-      </Box>
+        <Options 
+          Component="Change Password"
+          onPress={() => {navigation.navigate('ChangePassword')}}
+          />
+         </Box>
     </Box>
   );
 };
