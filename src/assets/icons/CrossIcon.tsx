d74b4122fdd5eba0b4b0482cb20c8dea9b558456
santhanakaleeswari <144306457+santhanakaleeswari@users.noreply.github.com>
2025-01-29 17:14:05 +0530

    Streak page changes maded (#25)
    * StreakPage
    
    * Changes maded in Streak Page
    
    ---------
    
    Co-authored-by: Mentor Bridge <mentorbridgeindia@gmail.com>

---
+++ src/assets/icons/CrossIcon.tsx
@@ -0,0 +1,9 @@
+import {Icon} from 'native-base';
+import React from 'react';
+import {Path} from 'react-native-svg';
+
+export const CrossIcon = () => (
+  <Icon width="15" height="15" viewBox="0 -960 960 960" fill="none" size="4" color='red.800'>
+    <Path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+  </Icon>
+);
