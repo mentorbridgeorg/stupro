e7dd7c3724835f98c6729dfa13555051dc870e49
sakthipriya2326 <sakthikannan823@gmail.com>
2025-01-30 14:52:24 +0530

    added url to endpoints
    added api call url for signIn and signUp add add regex for signIn

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,4 +1,4 @@
-export const REGISTER_ENDPOINT = 'account/sign-up';
-export const REGISTER_SOCIAL_ENDPOINT = 'account/social-signup';
-export const LOGIN_ENDPOINT = 'account/sign-in';
-export const LOGIN_SOCIAL_ENDPOINT = 'account/social-signin';
+export const REGISTER_ENDPOINT = 'http://ec2-35-87-21-24.us-west-2.compute.amazonaws.com:8092/sign-up';
+export const REGISTER_SOCIAL_ENDPOINT = 'http://ec2-35-87-21-24.us-west-2.compute.amazonaws.com:8092/social-signup';
+export const LOGIN_ENDPOINT = 'http://ec2-35-87-21-24.us-west-2.compute.amazonaws.com:8092/sign-in';
+export const LOGIN_SOCIAL_ENDPOINT = 'http://ec2-35-87-21-24.us-west-2.compute.amazonaws.com:8092/social-signin';
