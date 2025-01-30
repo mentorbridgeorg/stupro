b26d96392bcd77167ed269bce4e22e4751b58975
kiruthii <kiruthikaganesan757@gmail.com>
2025-01-30 10:55:18 +0530

    push notification

--- src/pages/Preferences/Preferences.tsx
+++ src/pages/Preferences/Preferences.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, ScrollView, Pressable, Alert } from 'react-nati
 import LinearGradient from 'react-native-linear-gradient';
 import axios from 'axios';
 
+
 export const Preferences = () => {
   const [selectedPreferences, setSelectedPreferences] = useState<string[]>([]);
   const [preferences, setPreferences] = useState([]);
@@ -100,70 +101,7 @@ useEffect(()=>{
   );
 };
 
-const styles = StyleSheet.create({
-  gradientBackground: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'black',
-    textAlign: 'center',
-    marginBottom: 20,
-    marginTop: 20,
-  },
-  scrollContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-  },
-  defaultButton: {
-    borderRadius: 20,
-    margin: 5,
-    overflow: 'hidden',
-  },
-  selectedButton: {
-    borderWidth: 2,
-    borderColor: '#FFD700',
-  },
-  buttonGradient: {
-    borderRadius: 20,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-  },
-  defaultButtonText: {
-    color: 'black',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
 
-  nextButton: {
-    backgroundColor: 'white',
-    paddingVertical: 10,
-    paddingHorizontal: 60,
-    borderRadius: 50,
-    alignSelf: 'center',
-    marginTop: 20,
-    marginBottom: 30,
-  },
-  nextButtonText: {
-    color: 'black',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  limitMessage: {
-    textAlign: 'center',
-    marginBottom: 15,
-    color: 'black',
-    fontSize: 18,
-  },
-});
 
 
 
