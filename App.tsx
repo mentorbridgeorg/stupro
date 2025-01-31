09104ef232d22f5c47e68c694c0426f744d5191e
sakthipriya2326 <sakthikannan823@gmail.com>
2025-01-31 16:05:47 +0530

    changes

--- App.tsx
+++ App.tsx
@@ -3,24 +3,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {NativeBaseProvider} from 'native-base';
 import {PostHogProvider} from 'posthog-react-native';
 import React from 'react';
-<<<<<<< HEAD
 import {theme} from './src/theme';
 
 require('./ReactotronConfig');
-=======
-import {SafeAreaView} from 'react-native';
-import {ChangePassword, SuccessPage} from './src/pages/ChangePassword';
-import {theme} from './src/theme';
-import {SignUp} from './src/pages/SignUp/SignUp';
-import {SignIn} from './src/pages/SignIn';
-import {Preferences} from './src/pages/Preferences';
-import { FeedbackPage } from './src/pages/Feedback';
->>>>>>> parent of 4820360 (homepage add)
 
 function App(): React.JSX.Element {
   return (
     <NativeBaseProvider theme={theme}>
-<<<<<<< HEAD
       <NavigationContainer>
         <PostHogProvider
           apiKey="phc_FlcrWuBmMaAMVqmYmuJ03BjVoCTomT6ckOxOOTaDWMl"
@@ -30,12 +19,6 @@ function App(): React.JSX.Element {
           <RootStack />
         </PostHogProvider>
       </NavigationContainer>
-=======
-      <SafeAreaView>
-        {/* <ChangePassword/> */}
-        <FeedbackPage/>
-      </SafeAreaView>
->>>>>>> parent of 4820360 (homepage add)
     </NativeBaseProvider>
   );
 }
