d3306087b59b93daf3fb061b3719678b357f5847
SBKrishnan <balakrishnan19211@gmail.com>
2025-02-06 12:37:00 +0530

    adding animation in profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,8 +1,8 @@
 import {useNavigation} from '@react-navigation/native';
 import {Box, VStack} from 'native-base';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import Options from './components/Options';
-import {Image, TouchableOpacity} from 'react-native';
+import {Animated, Image, TouchableOpacity} from 'react-native';
 import {EditIcon} from '../../assets/icons/EditIcon';
 
 export const Profile = () => {
@@ -19,6 +19,25 @@ export const Profile = () => {
   const handleBg: () => any = () => {
     return Position.find(findValue => findValue.value === role)?.background;
   };
+
+  const opacity = useRef(new Animated.Value(0)).current;
+  const translateY = useRef(new Animated.Value(50)).current;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 600, // Animation duration
+        useNativeDriver: true,
+      }),
+      Animated.spring(translateY, {
+        toValue: 0,
+        damping: 14, // Controls bounce effect
+        stiffness: 100, // Adjust stiffness for smoothness
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
 
   return (
     <Box flex={1}>
@@ -37,26 +56,33 @@ export const Profile = () => {
       </Box>
       // name, designation, role
       <Box position={'absolute'} top={100} left={0} right={0} p={5} ml={2}>
-        <Box _text={{color: 'black', fontSize: 50, fontWeight: '700'}}>
-          {name}
-        </Box>
-        <Box _text={{color: 'black', fontSize: 20, fontWeight: '400'}}>
-          {designation}
-        </Box>
-        <Box
-          bg={handleBg()}
-          borderRadius={10}
-          mt={2}
-          maxWidth={20}
-          p={2}
-          _text={{
-            color: 'black',
-            fontSize: 15,
-            fontWeight: '500',
-            textAlign: 'center',
-          }}>
-          {role}
-        </Box>
+        <Animated.View style={{opacity, transform: [{translateY}]}}>
+          <Box _text={{color: 'black', fontSize: 50, fontWeight: '700'}}>
+            {name}
+          </Box>
+        </Animated.View>
+        <Animated.View style={{opacity, transform: [{translateY}]}}>
+          <Box _text={{color: 'black', fontSize: 20, fontWeight: '400', pl: 1}}>
+            {designation}
+          </Box>
+        </Animated.View>
+
+        <Animated.View style={{opacity, transform: [{translateY}]}}>
+          <Box
+            bg={handleBg()}
+            borderRadius={10}
+            mt={2}
+            maxWidth={20}
+            p={2}
+            _text={{
+              color: 'black',
+              fontSize: 15,
+              fontWeight: '500',
+              textAlign: 'center',
+            }}>
+            {role}
+          </Box>
+        </Animated.View>
       </Box>
       // Settings
       <VStack marginX={7} mb={20} space={2}>
