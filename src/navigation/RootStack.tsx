e246fd4bdb8a2e5d6aa4f473c3f253f58e7d4809
SBKrishnan <balakrishnan19211@gmail.com>
2025-02-14 11:17:25 +0530

    Update Landing Page

--- src/navigation/RootStack.tsx
+++ src/navigation/RootStack.tsx
@@ -1,9 +1,6 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 
-
-
-
 import {ResetPassword} from '@/pages/ResetPassword';
 import {ChangePassword} from '@pages/ChangePassword';
 
@@ -11,19 +8,28 @@ import {Categories} from '@/pages/Categories/Categories';
 
 import {NavigationBar} from '@modules/navigationBar/navigationBar';
 import {View} from 'react-native';
+import {HomePage, SwipeCards} from '@/pages/Home';
+import EditProfile from '@/pages/Profile/EditProfile';
+import {Profile} from '@/pages/Profile';
+import {Preferences} from '@/pages/Preferences';
+import {SignIn} from '@/pages/SignIn';
+import {SignUp} from '@/pages/SignUp';
+import {ForgotPassword} from '@/pages/ForgotPassword';
+import {FeedbackPage} from '@/pages/Feedback';
+import Onboarding from '@/pages/LandingPage/Onboarding';
 
 const Stack = createNativeStackNavigator();
 
 export const RootStack = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Profile"
+      initialRouteName="Onboarding"
       screenOptions={{
         headerTitleAlign: 'center',
         headerShadowVisible: false,
         headerShown: false,
       }}>
-        <Stack.Screen name="HomePage" component={HomePage} />
+      <Stack.Screen name="HomePage" component={HomePage} />
       <Stack.Screen name="SwipeCards" component={SwipeCards} />
       <Stack.Screen name="EditProfile" component={EditProfile} />
       <Stack.Screen name="Profile" component={Profile} />
@@ -34,6 +40,7 @@ export const RootStack = () => {
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       <Stack.Screen name="FeedbackPage" component={FeedbackPage} />
       <Stack.Screen name="ChangePassword" component={ChangePassword} />
+      <Stack.Screen name="Onboarding" component={Onboarding} />
     </Stack.Navigator>
   );
 };
